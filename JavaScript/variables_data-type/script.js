4ca4957aa5aa3f10325049bf7561fe07808a7f4b
dominion-30 <abiolaayobamidele@gmail.com>
2024-02-16 00:27:05 +0100

    commit changes

--- JavaScript/variables_data-type/script.js
+++ JavaScript/variables_data-type/script.js
@@ -1,2 +1,66 @@
+//This is a single line of code
+
+// console.log(50);
+
 const x = 5000;
-console.log(x);
+// console.log(x);
+// This is using a single line comment for multiline
+// console.log(20);
+// console.log(40);
+// console.log(50);
+
+/*
+This is a multiline comment
+console.log(50);
+console.log(70);
+console.log(5000);
+*/
+
+// ways to declare a variable
+/* var
+let and const*/
+
+let firstName = "John";
+let lastName = "Doe";
+console.log(firstName, lastName);
+
+let age = 30;
+console.log(age);
+
+age = 31;
+
+console.log(age);
+
+let score;
+
+score = 1;
+
+console.log(score);
+
+if(true){
+    score = score + 1;
+}
+
+console.log(score);
+console.log(score);
+
+// alert(firstName);
+// alert(lastName);
+
+const num = 5;
+
+// num = 5;
+console.log(num);
+
+let a, b, c;
+console.log(a);
+a =1;
+b = 2;
+c = 3;
+
+console.log(a);
+const d = 7; e = 8; f = 9;
+
+console.log(d,e,f);
+
+
