a00473a416ac60c4f494fe53b4a66a3482700acb
dominion-30 <abiolaayobamidele@gmail.com>
2024-04-18 22:21:24 +0100

    add files

---
+++ JavaScript/variables_data-type/03_variables/script.js
@@ -0,0 +1,35 @@
+// Ways to declare a variable
+// var, let, const
+
+let firstName = "John";
+let lastName = "Doe";
+
+console.log( firstName, lastName);
+
+let age = 30;
+
+console.log(age);
+
+// Reassinging variables
+
+age = 31;
+
+console.log(age);
+
+let score;
+
+score = 1;
+if(true){
+    score  = score + 1;
+}
+
+console.log(score);
+
+const x = 100;
+console.log(x);
+
+
+const arr = [1,2,3,4];
+arr.push(5);
+
+console.log(arr);
