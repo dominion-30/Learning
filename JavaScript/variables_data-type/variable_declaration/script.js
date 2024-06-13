54f6b530ddad2dd60db6fb2c02d7c2f4950a34f2
dominion-30 <abiolaayobamidele@gmail.com>
2024-06-13 22:55:56 +0200

    commit changes

---
+++ JavaScript/variables_data-type/variable_declaration/script.js
@@ -0,0 +1,41 @@
+let firstName = "John";
+let lastName = "Doe";
+
+console.log(firstName, lastName);
+
+let age = 30;
+
+console.log(age);
+
+// Re-assigning variables
+
+age = 31;
+console.log(age);
+
+let score;
+
+score = 1;
+
+if (true){
+    score = score + 1;
+}
+
+console.log(score);
+
+
+const x = 100;
+
+// x = 200;
+// console.log(x);
+
+let year;
+
+
+const arr = [1,2,3,4];
+
+arr.push(5);
+
+console.log(arr);
+
+let a = 5, b = 10, c = 15;
+console.log(a,b,c);
