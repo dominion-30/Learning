b778e8b10ab97f0bf96d78a7e29f80765911382d
dominion-30 <abiolaayobamidele@gmail.com>
2024-06-13 22:00:41 +0200

    commit changes

--- JavaScript/variables_data-type/console_02/script.js
+++ JavaScript/variables_data-type/console_02/script.js
@@ -1,12 +1,16 @@
-// Single line comment.
-//This is a single line of code
-// console.log("This is a single line of code");
+// This is a single line of code
+// console.error(45);
 
-// Multi-line comment
 /*
-const x = 50;
-console.log(x);
+Multi-line comment
 
-const num = x + 500;
-console.log(num);
+console.error(100);
+console.error("Hello World");
+
+let name = "Ayo";
+console.log(name);
 */
+console.error("Where are you from?");
+console.error("Where are you from?");
+console.error("Where are you from?");
+console.error("Where are you from?");
