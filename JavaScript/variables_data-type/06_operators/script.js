81d78f93c4c36384e59cd6877df37c1d686463cd
dominion-30 <abiolaayobamidele@gmail.com>
2024-06-21 20:46:29 +0200

    commit changes

--- JavaScript/variables_data-type/06_operators/script.js
+++ JavaScript/variables_data-type/06_operators/script.js
@@ -1,94 +1,48 @@
-// // 1. Arithemetic Operators
+// Arithemetic Operators
+let x;
 
-// let x ;
+x = 5 + 5;
+x = 5 - 5;
+x = 5 * 5;
+x = 5 / 5;
+x = 7 % 5;
 
-// x = 5 + 5;
-// x = 5 - 5;
-// x = 5 * 5;
-// x = 5 / 5;
-// // modulus (remainder)
+// Concatenation
 
-// x = 7 % 5;
+x = "Hello" +" "+ "World"
 
-// // Concatenation
+// Exponent operator
+x = 2**3
 
-// x = "Hello"
-// x = x + " World";
-
-// // Exponent
-// x = 5 **3;
-// // Increment 
-// x = 1;
+// Increment
+x = 1;
 // x = x + 1;
-// ++x;
-// x++;
-// ++x;
+x++;
+
+// decrement
 // x = x - 1;
-// --x;
 // x--;
 
-// // Assignment operator
-// x = 10;
-// x += 5;
-// x -=5;
-// x *=5;
-// x /=5;
-// x %=5;
-// x **=5;
+// Assignment operators
+x = 10;
 
+x+=5;
+x-=5;
+x*=5;
+x/=5;
+x%=5;
 
-// // Comparison operator
-// // the single equal to only evaluates the values while 
-// // the triple equal to evaluates also the data type
-// x  = 2 == 2;
-// x = 2 === '2';
-// console.log(x);
+// comparison operators
+x = 2 == 2;
+x = 2 == 4;
 
-// // Type Coercion
+x = 2 === "2"
 
+x = 2!=="2";
 
+x = 10 > 5;
+x = 10 < 5;
+x = 10 <= 5;
+x = 10 >= 5;
 
-// let x;
-// x = 5+"5";
-// x = 5 + Number("5");
-
-// x = 5 * "5";
-
-// x = 5 + null;
-
-// x = Number(null);
-
-// x = Number(true);
-// x = Number(false);
-// console.log(x, typeof x);
-
-// let x;
-// const num = new Number(7.62348);
-
-// x = num.toString()
-// x = num.toString().length
-// x = num.toFixed(2)
-// x = num.toPrecision(2)
-// x = num.toExponential(2)
-
-// x = num.toLocaleString("en-US")
-// x = num.valueOf()
-
-// x = Number.MAX_VALUE;
-// x = Number.MIN_VALUE;
-// console.log(x, typeof x);
-// let x = "Abiola Ayobamidele";
-// let y = "Drama at the Console"
-// console.log(x,y)
-let x;
-x = Math.sqrt(9)
-x = Math.abs(-10);
-x = Math.round(6.42);
-x = Math.ceil(8.1);
-x = Math.pow(5,3);
-x = Math.pow(2,3);
-x = Math.min(5,4.5);
-x = Math.max(2.36,6.9);
-x = Math.floor(Math.random() * 11);
-// x = Math.floor(7.9);
 console.log(x);
