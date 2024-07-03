09bacd28a4fbf4d282eab4e7068244ee503f981d
dominion-30 <abiolaayobamidele@gmail.com>
2024-07-03 20:46:54 +0200

    commit changes

---
+++ JavaScript/variables_data-type/05_type_conversion/script.js
@@ -0,0 +1,37 @@
+let amount = "hello";
+
+// Covert string to a number
+// using parseInt
+
+// amount = parseInt(amount);
+
+// using the unary operator
+
+// amount = +amount
+
+// using the Number key word
+
+// amount = Number(amount)
+
+
+// Covert number to a string
+
+// amount = amount.toString();
+
+// Covert string to decimal
+// amount= parseFloat(amount);
+
+// convert number to boolean
+// console.log(amount);
+// amount = Boolean(amount)
+// amount = amount + 1
+
+amount = parseInt(amount);
+// console.log(Math.sqrt(-1));
+// console.log(1 + NaN);
+// console.log(50 / NaN);
+// console.log(undefined  + undefined);
+console.log("foo" + 3);
+console.log("faa" * 3);
+
+console.log(amount, typeof amount);
