5364da87a0cd585adc0cc018abfeaf838e6d4fb7
dominion-30 <abiolaayobamidele@gmail.com>
2024-07-18 20:25:24 +0200

    add changes

--- JavaScript/arrays/script.js
+++ JavaScript/arrays/script.js
@@ -70,38 +70,65 @@
 
 // console.log(x, arr);\\\
 
-let x;
-const fruits = ["apple","pear", "orange"];
+// let x;
+// const fruits = ["apple","pear", "orange"];
 
-const berries = ["strawberry", "blueberry", "rasberry"];
+// const berries = ["strawberry", "blueberry", "rasberry"];
 
 // x = fruits.push(berries);
-x = fruits[fruits.length-1][1];
+// x = fruits[fruits.length-1][1];
 
-const allFruits = [fruits, berries];
-x = allFruits;
-x = allFruits[1][0];
+// const allFruits = [fruits, berries];
+// x = allFruits;
+// x = allFruits[1][0];
 
-x = fruits.concat(berries)
+// x = fruits.concat(berries)
 
 // Spread operator (...)
 
-x = [...  fruits, ... berries];
+// x = [...  fruits, ... berries];
 
 // Flatten array
 
 
-const arr= [1,2,[3,4],5,[6,7], 8];
-x = arr.flat();
+// const arr= [1,2,[3,4],5,[6,7], 8];
+// x = arr.flat();
 
 // Static methods on array Objects
 
-x = Array.isArray();
+// x = Array.isArray();
 
-x = Array.from("12345678");
+// x = Array.from("12345678");
 
-const a = 1;
-const b = 2;
-const c = 3;
-x = Array.of(a,b,c)
-console.log( x);
+// const a = 1;
+// const b = 2;
+// const c = 3;
+// x = Array.of(a,b,c)
+// console.log( x);
+
+// Challenge
+
+// Challenge 1
+const arr = [1,2,3,4,5];
+arr.push(6);
+arr.unshift(0);
+arr.reverse();
+console.log(arr);
+
+// Challenge 2
+
+// solution 1
+const arr1  = [1,2,3,4,5];
+const arr2 = [5,6,7,8,9,10];
+// let x ;
+// x = arr2.splice(0,1);
+// const arr3 = arr1.concat(arr2);
+// console.log(x, arr2, arr3);
+
+// solution 2
+
+let y;
+
+y = arr1.slice(0,arr1.length-1);
+const arr3 = [...y, ...arr2];
+console.log( arr3);
