b2f6b5255b99220a993897cdcbf7e9aefaca86a2
dominion-30 <abiolaayobamidele@gmail.com>
2024-07-18 20:46:25 +0200

    add changes

--- JavaScript/arrays/script.js
+++ JavaScript/arrays/script.js
@@ -109,17 +109,17 @@
 // Challenge
 
 // Challenge 1
-const arr = [1,2,3,4,5];
-arr.push(6);
-arr.unshift(0);
-arr.reverse();
-console.log(arr);
+// const arr = [1,2,3,4,5];
+// arr.push(6);
+// arr.unshift(0);
+// arr.reverse();
+// console.log(arr);
 
 // Challenge 2
 
 // solution 1
-const arr1  = [1,2,3,4,5];
-const arr2 = [5,6,7,8,9,10];
+// const arr1  = [1,2,3,4,5];
+// const arr2 = [5,6,7,8,9,10];
 // let x ;
 // x = arr2.splice(0,1);
 // const arr3 = arr1.concat(arr2);
@@ -127,8 +127,31 @@ const arr2 = [5,6,7,8,9,10];
 
 // solution 2
 
-let y;
+// let y;
 
-y = arr1.slice(0,arr1.length-1);
-const arr3 = [...y, ...arr2];
-console.log( arr3);
+// y = arr1.slice(0,arr1.length-1);
+// const arr3 = [...y, ...arr2];
+// console.log( arr3);
+
+
+// Object Literal
+// to access properties from the  object literal, we could use the dot notation. i.e person.name put in a variable or the bracket notation, i.e person["age"]
+
+let x;
+
+const person = {
+    name: "John Doe",
+    age: 30,
+    isAdmin:true,
+    address:{
+        street: "123 Main st",
+        city:"Boston",
+        state:"MA",
+    },
+
+};
+x = person.name;
+x = person["name"];
+x = person.address.street;
+
+console.log(x);
