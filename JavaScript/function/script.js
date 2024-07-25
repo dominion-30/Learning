d6c490cb6b100534e796e697cb130158f67fd3a6
dominion-30 <abiolaayobamidele@gmail.com>
2024-07-25 16:41:05 +0200

    add files

---
+++ JavaScript/function/script.js
@@ -0,0 +1,12 @@
+function sayHello(){
+    console.log("Hello World");
+}
+
+sayHello();
+
+// parameter are the variables in the parenthesis, while argument is/are the data type that is used when the function is to be called.
+function add(num1, num2){
+    console.log(num1 + num2);
+}
+
+add(50,50);
