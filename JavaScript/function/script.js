d22de4fdd2008da3d50a82644eea6b22ff2b8755
dominion-30 <abiolaayobamidele@gmail.com>
2024-07-26 18:57:17 +0200

    add files

--- JavaScript/function/script.js
+++ JavaScript/function/script.js
@@ -143,49 +143,84 @@
 
 // Block Scope
 
-const x = 100;
+// const x = 100;
 
 // console.log(x);
 
-if(true){
-    const y = 200;
-    console.log(x + y);
-}
+// if(true){
+//     const y = 200;
+//     console.log(x + y);
+// }
 // const y = 1
 
 // console.log(x +y);
 
-for( let i = 0; i<= 10; i++){
-    console.log(i);
-}
+// for( let i = 0; i<= 10; i++){
+//     console.log(i);
+// }
 
 // console.log(i);
 
-if(true){
-    const a = 500;
-    let b = 600;
-    var c = 700;
-}
+// if(true){
+//     const a = 500;
+//     let b = 600;
+//     var c = 700;
+// }
 
 // console.log(c);
-console.log("testing var");
+// console.log("testing var");
 
-for(var i = 0; i<=10; i++){
+// for(var i = 0; i<=10; i++){
 
-    console.log(i);
+//     console.log(i);
 
-}
-console.log(i);
+// }
+// console.log(i);
 
-function running(){
-    var b = 1000;
-    console.log(b);
-}
+// function running(){
+//     var b = 1000;
+//     console.log(b);
+// }
 
-running()
+// running()
 // console.log(b+ 1);
 
-var testing = 4;
-let test = 14;
-const tests = 140;
-console.log(window);
+// var testing = 4;
+// let test = 14;
+// const tests = 140;
+// console.log(window);
+
+// function first(){
+//     const x = 100;
+
+//     function second(){
+//         const y = 200;
+//         console.log(x + y);
+
+//     }
+    
+//     second();
+//     console.log(y);
+// }
+
+// first();
+
+
+// Funtion Declaration
+// const result = addDollarSign(500);
+// console.log(result);
+// function addDollarSign(value){
+//     return `$${value}`;
+// };
+
+
+
+
+
+// function expression
+
+const addPlusSign = function (value){
+    return "+" + value;
+}
+
+console.log(addPlusSign(100));
