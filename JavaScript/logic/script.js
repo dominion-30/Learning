0c34711ed73152a9ded7b5d457593ff4624f1acb
dominion-30 <abiolaayobamidele@gmail.com>
2024-08-06 00:21:10 +0200

    add changes

--- JavaScript/logic/script.js
+++ JavaScript/logic/script.js
@@ -1,31 +1,31 @@
-if(true){
-    console.log("This is true");
-}
+// if(true){
+//     console.log("This is true");
+// }
 
-if(false){
-    console.log("This is Not true");
-}
+// if(false){
+//     console.log("This is Not true");
+// }
 
-const x = 10;
-const y = 5;
+// const x = 10;
+// const y = 5;
 
-if(x >=  y){
-    console.log(`${x} is greater than or equal to ${y}`);
+// if(x >=  y){
+//     console.log(`${x} is greater than or equal to ${y}`);
 
-}
+// }
 
-if(x ===  y){
-    console.log(`${x} = ${y}`);
+// if(x ===  y){
+//     console.log(`${x} = ${y}`);
 
-}
-else{
-    console.log(`${x} is not equal to ${y}`);
-}
+// }
+// else{
+//     console.log(`${x} is not equal to ${y}`);
+// }
 
-if(x!==y){
-    const z = 20;
-    console.log(`${z} is 20`);
-}
+// if(x!==y){
+//     const z = 20;
+//     console.log(`${z} is 20`);
+// }
 
 // console.log(z);
 
@@ -36,8 +36,8 @@ if(x!==y){
 // d = new Date(2021, 6, 10, 22,5,0);
 // console.log( d);
 
-const d = new Date(10,30,2022,17,00,0);
-const hour = d.getHours();
+// const d = new Date(10,30,2022,17,0,0);
+// const hour = d.getHours();
 // console.log(hour);
 
 // if(hour < 12){
@@ -53,45 +53,68 @@ const hour = d.getHours();
 
 // Nested if
 
-if(hour < 12){
-    console.log("Good Morning");
+// if(hour < 12){
+//     console.log("Good Morning");
     
-    if(hour === 6){
-        console.log("Wake Up");
-    }
-}
-else if (hour<=18){
-    console.log("Good Afternoon");
+//     if(hour === 6){
+//         console.log("Wake Up");
+//     }
+// }
+// else if (hour<=18){
+//     console.log("Good Afternoon");
 
-}
-else{
-    console.log("Good Night");
+// }
+// else{
+//     console.log("Good Night");
 
-    if(hour >= 20){
-        console.log("Bed Time");
-    }
-}
+//     if(hour >= 20){
+//         console.log("Bed Time");
+//     }
+// }
 
-if(hour >= 7 && hour < 12)  {
+// if(hour >= 7 && hour < 12)  {
    
 
-    console.log("It is work time");
+//     console.log("It is work time");
     
-}
+// }
 
-else if (hour >=12 && hour < 13 || hour >=15 && hour <16){
-    console.log("Break Time");
-}
+// else if (hour >=12 && hour < 13 || hour >=15 && hour <16){
+//     console.log("Break Time");
+// }
 
-else{
-    if (hour >= 13 && hour < 15){
-        console.log("It is work time");
-    }
-    else if( hour >=16 && hour < 17){
-        console.log("it is work time");
-    }
-    else{
-        console.log("Office is closed");
-    }
-}
+// else{
+//     if (hour >= 13 && hour < 15){
+//         console.log("It is work time");
+//     }
+//     else if( hour >=16 && hour < 17){
+//         console.log("it is work time");
+//     }
+//     else{
+//         console.log("Office is closed");
+//     }
+// }
 
+// const d = new Date(2022, 1, 10, 8, 0, 0);
+const d = new Date('2022-04-10T08:00:00');
+
+const Getmonth = d.getMonth();
+
+let month = Getmonth + 1;
+
+switch(month){
+    case 1:
+        console.log("It is January");
+        break;
+    
+    case 2:
+        console.log("It is February");
+        break;
+
+    case 3:
+        console.log("It is March");
+        break;
+
+    default:
+        console.log("It is neither January, February or March");
+}
