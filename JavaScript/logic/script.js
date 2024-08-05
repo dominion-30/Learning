df2a372e1f09531f9e60814b9bc6100bd055f2b9
dominion-30 <abiolaayobamidele@gmail.com>
2024-08-05 22:36:57 +0200

    add changes

--- JavaScript/logic/script.js
+++ JavaScript/logic/script.js
@@ -28,3 +28,57 @@ if(x!==y){
 }
 
 // console.log(z);
+
+// let d;
+// d = new Date();
+// d = d.toString();
+
+// d = new Date(2021, 6, 10, 22,5,0);
+// console.log( d);
+
+const d = new Date(10,30,2022,12,30,0);
+const hour = d.getHours();
+// console.log(hour);
+
+// if(hour < 12){
+//     console.log("Good Morning");
+// }
+// else if (hour<=18){
+//     console.log("Good Afternoon");
+
+// }
+// else{
+//     console.log("Good Night");
+// }
+
+// Nested if
+
+if(hour < 12){
+    console.log("Good Morning");
+    
+    if(hour === 6){
+        console.log("Wake Up");
+    }
+}
+else if (hour<=18){
+    console.log("Good Afternoon");
+
+}
+else{
+    console.log("Good Night");
+
+    if(hour >= 20){
+        console.log("Bed Time");
+    }
+}
+
+if(hour >= 7 && hour < 17){
+    console.log("It is work time");
+    
+}
+else{
+    if (hour >=12 && hour < 13 || hour >=15 && hour <16){
+        console.log("Break Time");
+    }
+}
+
